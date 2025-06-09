eda866e22354edfe9bbf64b85d1934130fbfda09
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2025-06-09 14:20:19 +0300

    Vkalenski/added dotnet health check (#5)
    * Added dotnet web app with health check
    
    * Added dotnet health check project & dotnet redis project

---
+++ src/dotnet-redis-db/RedisAPI/Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+﻿#region Usings
+using RedisAPI.Models;
+#endregion
+
+namespace RedisAPI.Data
+{
+    public interface IPlatformRepo
+    {
+        void CreatePlatform(Platform plat);
+        Platform? GetPlatformById(string id);
+        IEnumerable<Platform?>? GetAllPlatforms();
+    }
+}
